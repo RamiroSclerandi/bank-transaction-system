6473dce01accf2887219eae48f18128e26cbc50f
Ramiro Sclerandi <ramirosclerandi1@gmail.com>
2026-05-12 14:39:51 -0300

    bugfix: modified env vars management

--- backend/tests/unit/services/test_customer_service.py
+++
@@ -1,5 +0,0 @@
-"""
-customer_service.py has been removed. Coverage lives in:
-  - tests/unit/services/test_auth_service.py  (login, logout)
-  - tests/unit/services/test_user_service.py  (register_customer)
-"""
